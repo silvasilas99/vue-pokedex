26185705005b22a1e351ce66b025c4bd5cd4a8f6
Silas Silva <silvasilas66@gmail.com>
2021-06-18 20:05:14 -0300

    Creating Favorites component

---
+++ src/components/Favorites.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1 class="overview-favorite-pokemon__title">
+            Your favorite Pokémon
+        </h1>
+        <router-link 
+            class="nes-btn"
+            to="/"
+        >
+            Go back
+        </router-link>
+        <div class="overview-favorite-pokemon" />
+    </div>
+</template>
+
+<style scoped>
+.overview-favorite-pokemon {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+}
+
+.overview-favorite-pokemon__title {
+    margin-top: 10px;
+    text-align: center;
+}
+</style>
