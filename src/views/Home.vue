e1a48336903d79493b27d46e47af5209fc68cd54
Silas Silva <silvasilas66@gmail.com>
2021-06-18 20:11:44 -0300

    Change directory structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="select-pokemon-page"> 
+        <img 
+            src="./../assets/logo-pixel.gif" 
+            alt="Pokélogo"
+        >
+        <h1>Home-page</h1>
+        <div class="select-pokemon-content" />
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
+    async created() {
+        const pokemonData = await this.getPokemonData()
+        this.setPokemonData(pokemonData)
+    },
+    methods: {
+        async getPokemonData() {
+            const data = await fetch (
+                'https://pokeapi.co/api/v2/pokemon?limit=151'
+            )
+            const json = await data.json()
+            return json.results
+        }
+    }
+}
 </script>
+
+<style scoped>
+.select-pokemon-page {
+    text-align: center;
+}
+
+.select-pokemon-content {
+    text-align: left;
+    display: flex;
+    justify-content: space-evenly;
+}
+</style>
